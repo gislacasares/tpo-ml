fdfb967b886ff62db451162481b737c7bd3c212d
Gaston Isla Casares <gislacasares@gmail.com>
2021-08-23 23:03:05 -0300

    Agregando registro

--- app.js
+++ app.js
@@ -19,3 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
+
+app.get('/registrarse', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
